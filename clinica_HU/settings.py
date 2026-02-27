cc3f0e48ff21305fbe1b0a56967c6e78d8c001b4
Kerwin <kerwinfidelqr@gmail.com>
2026-02-27 11:07:37 -0500

    Configuración de Gunicorn y ALLOWED_HOSTS para Render

--- clinica_HU/settings.py
+++ clinica_HU/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-latus7wwq=6q_n%y8&jh$0sa8f#%5mhy)pczi!p2zb&75zq5o^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -102,9 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-pe'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Lima'
 
 USE_I18N = True
 
