ad7e6f3f637eabd58b8d953f71e33c7413be209b
Kerwin <kerwinfidelqr@gmail.com>
2026-02-27 11:48:22 -0500

    Mejora del diseño inicial: interfaz de módulos añadida

--- clinica_HU/urls.py
+++ clinica_HU/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from clinica_HU import views  # <-- Importamos tus vistas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.inicio, name='inicio'), # <-- Ruta de la página principal
 ]
+
